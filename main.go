141921bd08297e2161345bb1900489fb06ec348a
endkarn <end_karn@hotmail.com>
2019-08-09 19:17:42 +0700

    Update using flag into main.go

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	profile := flag.String("profile", "default", "your profile")
+	weight := flag.Int("weight", 0, "your weight")
+
+	flag.Parse()
+
+	fmt.Printf("Hello, you are starting GO with %s profile. \n", *profile)
+
+	if *weight != 0 {
+		fmt.Printf("Your weight is %d kilogram, Let's do some fittest \n", *weight)
+	} else {
+		fmt.Printf("You may forget about setting weight before start.\n")
+	}
+
+	fmt.Println("tail:", flag.Args())
 }
